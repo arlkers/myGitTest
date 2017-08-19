cc12bd174b97d219ce225c1d56d0177d5f1dc0ec
arlkers <arlkers@163.com>
2017-08-20 03:50:02 +0800

    second commit

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,11 @@
+//2017/8/20
 #include <iostream>
-
+#include <ctime>
 int main()
 {
+    time_t t;  
+    time(&t);
     std::cout << "Hello Git!" << std::endl;
+    std::cout << ctime(&t) << std::endl;
     return 0;
 }
